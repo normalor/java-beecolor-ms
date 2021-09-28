86d839ed032d7e8e45d8fc5acb94ba746b9b0a68
Tung Nguyen <nntung@gmail.com>
2021-09-27 21:57:46 -0400

    Initilize Spring Boot project

---
+++ src/main/java/com/beecolorshop/api/BeeColorApiApplication.java
@@ -0,0 +1,13 @@
+package com.beecolorshop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeColorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeColorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beecolorshop/api/BeeColorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beecolorshop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeColorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
