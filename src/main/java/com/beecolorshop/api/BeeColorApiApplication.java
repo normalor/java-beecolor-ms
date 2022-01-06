28fb4c5f08d5a002342dcc2d52df6684a9eb2e8c
Tung Nguyen <nntung@gmail.com>
2022-01-05 22:40:37 -0500

    modify to run with Tomcat

--- src/main/java/com/beecolorshop/api/BeeColorApiApplication.java
+++ src/main/java/com/beecolorshop/api/BeeColorApiApplication.java
@@ -4,14 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class BeeColorApiApplication {
-
+public class BeeColorApiApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(BeeColorApiApplication.class, args);
 	}
-
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BeeColorApiApplication.class);
+    }
 }
+
+//@SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//public class BeeColorApiApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(BeeColorApiApplication.class, args);
+//	}
+//
+//}
+
--- src/main/java/com/beecolorshop/api/controller/ProductServiceController.java
+++ src/main/java/com/beecolorshop/api/controller/ProductServiceController.java
@@ -3,13 +3,20 @@ package com.beecolorshop.api.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RequestMapping("/product")
 public class ProductServiceController {
 
-	@GetMapping (value="test")
+	@GetMapping("/")
+	public String greeting() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/test")
 	public ResponseEntity<Object> getProduct() {
-		return new ResponseEntity<>("Hello world", HttpStatus.OK);
+		return new ResponseEntity<>("Hello world Test", HttpStatus.OK);
 	}
 }
